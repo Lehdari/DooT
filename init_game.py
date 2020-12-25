02bf4cf6d33930092c95889f11a110e6569df594
Miika Lehtimäki <miika.lehtimaki@gmail.com>
2020-12-25 21:03:47 +0200

    Move game object initialization to init_game.py

---
+++ init_game.py
@@ -0,0 +1,87 @@
+import vizdoom as vzd
+
+
+def init_game():
+    # Create DoomGame instance. It will run the game and communicate with you.
+    game = vzd.DoomGame()
+
+    # Now it's time for configuration!
+    # load_config could be used to load configuration instead of doing it here with code.
+    # If load_config is used in-code configuration will also work - most recent changes will add to previous ones.
+    # game.load_config("../../scenarios/basic.cfg")
+
+    # Sets path to additional resources wad file which is basically your scenario wad.
+    # If not specified default maps will be used and it's pretty much useless... unless you want to play good old Doom.
+    game.set_doom_scenario_path("wads/doom2.wad")
+
+    # Sets map to start (scenario .wad files can contain many maps).
+    game.set_doom_map("map01")
+
+    # Sets resolution. Default is 320X240
+    game.set_screen_resolution(vzd.ScreenResolution.RES_640X480)
+
+    # Sets the screen buffer format. Not used here but now you can change it. Default is CRCGCB.
+    game.set_screen_format(vzd.ScreenFormat.RGB24)
+
+    # Enables depth buffer.
+    game.set_depth_buffer_enabled(True)
+
+    # Enables labeling of in game objects labeling.
+    game.set_labels_buffer_enabled(True)
+
+    # Enables buffer with top down map of the current episode/level.
+    game.set_automap_buffer_enabled(True)
+
+    # Enables information about all objects present in the current episode/level.
+    game.set_objects_info_enabled(True)
+
+    # Enables information about all sectors (map layout).
+    game.set_sectors_info_enabled(True)
+
+    # Sets other rendering options (all of these options except crosshair are enabled (set to True) by default)
+    game.set_render_hud(True)
+    game.set_render_minimal_hud(False)  # If hud is enabled
+    game.set_render_crosshair(False)
+    game.set_render_weapon(True)
+    game.set_render_decals(True)  # Bullet holes and blood on the walls
+    game.set_render_particles(True)
+    game.set_render_effects_sprites(False)  # Smoke and blood
+    game.set_render_messages(False)  # In-game messages
+    game.set_render_corpses(True)
+    game.set_render_screen_flashes(True)  # Effect upon taking damage or picking up items
+
+    # Adds buttons that will be allowed.
+    game.add_available_button(vzd.Button.MOVE_LEFT)
+    game.add_available_button(vzd.Button.MOVE_RIGHT)
+    game.add_available_button(vzd.Button.ATTACK)
+
+    # Adds game variables that will be included in state.
+    game.add_available_game_variable(vzd.GameVariable.AMMO2)
+
+    # Causes episodes to finish after 200 tics (actions)
+    game.set_episode_timeout(200)
+
+    # Makes episodes start after 10 tics (~after raising the weapon)
+    game.set_episode_start_time(10)
+
+    # Makes the window appear (turned on by default)
+    game.set_window_visible(True)
+
+    # Turns on the sound. (turned off by default)
+    # game.set_sound_enabled(True)
+    # Because of some problems with OpenAL on Ubuntu 20.04, we keep this line commented,
+    # the sound is only useful for humans watching the game.
+
+	# Sets the living reward (for each move) to -1
+    game.set_living_reward(-1)
+
+    # Sets ViZDoom mode (PLAYER, ASYNC_PLAYER, SPECTATOR, ASYNC_SPECTATOR, PLAYER mode is default)
+    game.set_mode(vzd.Mode.PLAYER)
+
+    # Enables engine output to console.
+    #game.set_console_enabled(True)
+
+    # Initialize the game. Further configuration won't take any effect from now on.
+    game.init()
+
+    return game
