b7980e7e731f19473b9e2c1a326b3b34d14b7c65
Miika Lehtimäki <miika.lehtimaki@gmail.com>
2020-12-28 18:07:21 +0200

    Reward: Use distance delta as distance reward

--- reward.py
+++ reward.py
@@ -1,16 +1,22 @@
 import vizdoom as vzd
 import numpy as np
-import utils
+from utils import *
 
 class Reward():
     def __init__(self, player_start_pos):
         self.player_start_pos = player_start_pos
+        self.dist_start_prev = 0.0
     
     def get_sector_crossing_reward(self):
         return 0.0 # TODO
 
     def get_reward(self, game):
-        start_dist_reward = utils.get_player_dist_from_start(game, self.player_start_pos)
+    	# current distance from starting point
+        dist_start = np.linalg.norm(get_player_pos(game) - self.player_start_pos)
+        # starting distance reward given according to dis. delta
+        start_dist_reward = dist_start - self.dist_start_prev
+        self.dist_start_prev = dist_start
+
         living_reward = -1.0
 
         sector_crossing_reward = self.get_sector_crossing_reward() # TODO
