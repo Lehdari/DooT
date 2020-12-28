06ad073a1bdff061e3e80ede8c7925a532871c6d
Miika Lehtimäki <miika.lehtimaki@gmail.com>
2020-12-28 18:08:17 +0200

    Trainer: Use the state-action model

--- trainer.py
+++ trainer.py
@@ -2,6 +2,8 @@ from reward import Reward
 from model import Model
 import numpy as np
 import tensorflow as tf
+from collections import deque
+import random
 
 
 class Trainer:
@@ -9,13 +11,14 @@ class Trainer:
 		self.model = model
 		self.reward = reward
 
-		self.reward = reward
 		self.memory = deque(maxlen=2000)
 		self.gamma = 0.85
 		self.epsilon = 1.0
 		self.epsilon_min = 0.01
 		self.epsilon_decay = 0.995
 		self.learning_rate = 0.005
+
+		self.action_prev = np.zeros((15,))
 
 	"""
 	Perform one step;
@@ -26,23 +29,26 @@ class Trainer:
 	return: reward (1D float)
 	"""
 	def step(self, game):
-		state = game.get_state()
+		state_game = game.get_state()
 
 		#TODO: stack frames
-		screen_buf = state.screen_buffer
+		screen_buf = state_game.screen_buffer
 
-		"""
-		Epsilon-greedy algorithm
-		With probability epsilon choose a random action ("explore")
-		With probability 1-epsilon choose best known action ("exploit")
-		"""
+		# save the previous model state
+		state_prev = self.model.state.copy()
 
-		action = self.predict_action(np.expand_dims(screen_buf,0))
+		# advance the model state using new screen buffer and the previously taken action
+		self.model.advance(screen_buf, self.action_prev)
 
+		# Epsilon-greedy algorithm
+		# With probability epsilon choose a random action ("explore")
+		# With probability 1-epsilon choose best known action ("exploit")
 		self.epsilon *= self.epsilon_decay
 		self.epsilon = max(self.epsilon_min, self.epsilon)
 		if np.random.random() < self.epsilon:
-			action = self.get_random_action()
+			action = self.model.get_random_action() # make random action
+		else:
+			action = self.model.predict_action() # make action predicted from model state
 
 		# Intentionally ignore the reward the game gives
 		game.make_action(action)
@@ -50,11 +56,16 @@ class Trainer:
 		# Instead, use our own reward system
 		reward = self.reward.get_reward(game)
 
-		done = game.is_episode_finished()
-		bufu = None
-		if not done:
-			bufu = game.get_state().screen_buffer
-		self.remember(screen_buf, action, reward, bufu, done)
+		# save the action taken for next step
+		self.action_prev = action.copy()
+
+		print("reward: {}".format(reward))
+
+		# done = game.is_episode_finished()
+		# bufu = None
+		# if not done:
+		# 	bufu = game.get_state().screen_buffer
+		# self.remember(screen_buf, action, reward, bufu, done)
 
 		#self.replay()
 
