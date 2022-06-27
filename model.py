591fd290a5ab6604538f39ffc0486920fd21f42e
Lehdari <miika.lehtimaki@gmail.com>
2022-06-27 20:39:44 +0300

    Model: Implement learned sine encodings for image decoder

--- model.py
+++ model.py
@@ -185,6 +185,9 @@ class Model:
 		self.image_encs = []
 		self.image_enc_preds = []
 		self.states = []
+
+		np.random.seed(1507715517)
+		
 	
 	def save_episode_state_images(self, episode_id):
 		if len(self.image_encs) == 0:
@@ -526,7 +529,9 @@ class Model:
 	
 
 	def module_deconv(self, x, n1, n2, k1=(4,4), k2=(2,2), s1=(2,2), s2=(1,1),
-		p1="same", p2="same", activation="relu", bn2=True, alpha=0.001):
+		p1="same", p2="same", activation="relu", bn2=True, alpha=0.001,
+		initializer_primary=initializers.Orthogonal(0.5),
+		initializer_secondary=initializers.Orthogonal()):
 
 		#shortcut by linear upsampling
 		pool_x_size = s1[0]*s2[0]
@@ -538,12 +543,12 @@ class Model:
 			pool_x_size += k2[0]-1
 			pool_y_size += k2[1]-1
 		y = layers.UpSampling2D((pool_x_size, pool_y_size), interpolation="bilinear")(x)
-		y = layers.Conv2D(n2, (1, 1), kernel_initializer=initializers.Orthogonal(0.5), use_bias=False)(y)
+		y = layers.Conv2D(n2, (1, 1), kernel_initializer=initializer_primary, use_bias=False)(y)
 		y = layers.BatchNormalization(axis=-1,
 			beta_initializer = self.beta_initializer,
 			gamma_initializer = self.gamma_initializer)(y)
 
-		x = layers.Conv2DTranspose(n1, k1, padding=p1, kernel_initializer=initializers.Orthogonal(),
+		x = layers.Conv2DTranspose(n1, k1, padding=p1, kernel_initializer=initializer_secondary,
 			strides=s1, use_bias=False)(x)
 		x = layers.BatchNormalization(axis=-1,
 			beta_initializer = self.beta_initializer,
@@ -552,8 +557,10 @@ class Model:
 			x = layers.Activation(activations.tanh)(x)
 		elif activation == "relu":
 			x = activations.relu(x, alpha=alpha)
+		else:
+			pass
 
-		x = layers.Conv2DTranspose(n2, k2, padding=p2, kernel_initializer=initializers.Orthogonal(0.5),
+		x = layers.Conv2DTranspose(n2, k2, padding=p2, kernel_initializer=initializer_primary,
 			strides=s2, use_bias=False)(x)
 		x = layers.BatchNormalization(axis=-1,
 			beta_initializer = self.beta_initializer,
@@ -562,6 +569,8 @@ class Model:
 			x = layers.Activation(activations.tanh)(x)
 		elif activation == "relu":
 			x = activations.relu(x, alpha=alpha)
+		else:
+			pass
 
 		x = layers.Add()([x, y])
 		
@@ -656,6 +665,58 @@ class Model:
 		return x2
 
 	
+	def module_interpolated_sin(self, x, n=1, upscale_ratio=2, base_matrix_progression=2.0):
+		batch_size = tf.shape(x)[0]
+		
+		# s1 = int(np.sqrt(upscale_ratio))
+		# s2 = int(upscale_ratio/s1) # TODO error in case the result is not an integer
+
+		# # upsample input features with deconvolution module
+		# x = self.module_deconv(x, 8*n, 6*n,
+		# 	k1=(s1, s1), k2=(s2, s2), s1=(s1, s1), s2=(s2, s2), activation="tanh",
+		# 	initializer_primary=initializers.Orthogonal(0.5),
+		# 	initializer_secondary=initializers.Orthogonal(1.0))
+
+		x = layers.Conv2D(8*n, (1,1), activation="tanh",
+			kernel_initializer=initializers.Orthogonal(1.0))(x)
+		x = layers.UpSampling2D(size=(upscale_ratio, upscale_ratio), interpolation="bilinear")(x)
+
+		# translation coefficients
+		t = layers.Conv2D(2*n, (1,1), kernel_initializer=initializers.Orthogonal(1.0))(x)
+		# transformation matrix coefficients
+		x = layers.Conv2D(4*n, (1,1), kernel_initializer=initializers.Orthogonal(0.1))(x)
+
+		out_rows = x.get_shape().as_list()[1]
+		out_cols = x.get_shape().as_list()[2]
+		
+		# pixel coordinates meshgrid
+		px, py = tf.meshgrid(
+			tf.linspace(-out_cols+0.5, out_cols-0.5, out_cols),
+			tf.linspace(-out_rows+0.5, out_rows-0.5, out_rows)
+		)
+		p = tf.concat([tf.expand_dims(px*np.pi*0.5, axis=2), tf.expand_dims(py*np.pi*0.5, axis=2)], axis=2)
+		p = tf.repeat(tf.expand_dims(p, axis=2), repeats=n, axis=2)
+
+		matmul_batch_shape = (batch_size, out_rows, out_cols, n)
+
+		# reshape coefficients into matrices
+		x = tf.reshape(x, [*matmul_batch_shape, 2, 2])
+
+		# create gaussian base matrices with geometrically decreasing amplitude
+		base_matrices = tf.convert_to_tensor(
+			np.stack([np.random.normal(0.0, 1.0/(base_matrix_progression**i), (2, 2))
+				for i in range(n)], axis=0), dtype=tf.float32)
+		
+		# add learned matrix coefficients to base matrices
+		x = tf.broadcast_to(base_matrices, [*matmul_batch_shape, 2, 2]) + x*1.0e-20
+
+		# transform and final reshape
+		x = tf.linalg.matvec(x, tf.broadcast_to(p, [*matmul_batch_shape, 2]))
+		x = tf.reshape(x, [batch_size, out_rows, out_cols, 2*n])
+
+		# translation and sine activation
+		x = tf.math.sin(x + t)
+		return x
 
 	def create_image_encoder_model(self, feature_multiplier=1):
 		self.model_image_encoder_i_image = keras.Input(shape=(240, 320, 3))
@@ -757,39 +818,79 @@ class Model:
 		self.model_image_decoder_i_image_enc = keras.Input(shape=(self.image_enc_size))
 
 		x = layers.Reshape((15, 20, -1))(self.model_image_decoder_i_image_enc)
-		e = self.create_positional_embedding((15,20), 5)
-		x = layers.Concatenate()([x, tf.broadcast_to(e, [tf.shape(x)[0], *e.shape])])
+		#e = self.create_positional_embedding((15,20), 5)
+		#x = layers.Concatenate()([x, tf.broadcast_to(e, [tf.shape(x)[0], *e.shape])])
 
-		x = self.module_conv(x, 32*feature_multiplier, 64*feature_multiplier,
-			k1=(3,3), k2=(1,3), s1=(1,1), s2=(1,1), activation="tanh") #20x15
+		# x1 = self.module_conv(x, 32*feature_multiplier, 64*feature_multiplier,
+		# 	k1=(3,3), k2=(1,3), s1=(1,1), s2=(1,1), activation="tanh") #20x15
 
+		s = self.module_interpolated_sin(x, n=11, upscale_ratio=2,
+			base_matrix_progression=2.0**(1.0/2.0))
 		x = self.module_deconv(x, 128*feature_multiplier, 64*feature_multiplier,
 			k1=(2,2), k2=(2,2), activation="tanh") #40x30
-		e = self.create_positional_embedding_mesh((x.shape[1], x.shape[2]))
-		x = layers.Concatenate()([x, tf.broadcast_to(e, [tf.shape(x)[0], *e.shape])])
+		# e = self.create_positional_embedding_mesh((x.shape[1], x.shape[2]))
+		# x = layers.Concatenate()([x, tf.broadcast_to(e, [tf.shape(x)[0], *e.shape])])
+		x = layers.Concatenate()([x, s])
 
+		s = self.module_interpolated_sin(x, n=4, upscale_ratio=2,
+			base_matrix_progression=2.0**(1.0/4.0))
 		x = self.module_deconv(x, 64*feature_multiplier, 32*feature_multiplier,
 			k1=(2,2), k2=(2,2), activation="tanh") #80x60
-		e = self.create_positional_embedding_mesh((x.shape[1], x.shape[2]))
-		x = layers.Concatenate()([x, tf.broadcast_to(e, [tf.shape(x)[0], *e.shape])])
+		# e = self.create_positional_embedding_mesh((x.shape[1], x.shape[2]))
+		# x = layers.Concatenate()([x, tf.broadcast_to(e, [tf.shape(x)[0], *e.shape])])
+		x = layers.Concatenate()([x, s])
 
+		# s1 = self.module_interpolated_sin(x1, 6, 4, np.sqrt(2.0))
+		# x2 = layers.Concatenate()([x, s1])
+
+		s = self.module_interpolated_sin(x, n=4, upscale_ratio=2,
+			base_matrix_progression=2.0**(1.0/4.0))
 		x = self.module_deconv(x, 32*feature_multiplier, 16*feature_multiplier,
 			k1=(2,2), k2=(2,2), activation="tanh") #160x120
-		e = self.create_positional_embedding_mesh((x.shape[1], x.shape[2]))
-		x = layers.Concatenate()([x, tf.broadcast_to(e, [tf.shape(x)[0], *e.shape])])
+		# e = self.create_positional_embedding_mesh((x.shape[1], x.shape[2]))
+		# x = layers.Concatenate()([x, tf.broadcast_to(e, [tf.shape(x)[0], *e.shape])])
+		x = layers.Concatenate()([x, s])
 
+		s = self.module_interpolated_sin(x, n=4, upscale_ratio=2,
+			base_matrix_progression=2.0**(1.0/4.0))
 		x = self.module_deconv(x, 16*feature_multiplier, 8*feature_multiplier,
 			k1=(2,2), k2=(2,2), activation="tanh") #320x240
-		e = self.create_positional_embedding_mesh((x.shape[1], x.shape[2]))
-		x = layers.Concatenate()([x, tf.broadcast_to(e, [tf.shape(x)[0], *e.shape])])
+		# e = self.create_positional_embedding_mesh((x.shape[1], x.shape[2]))
+		# x = layers.Concatenate()([x, tf.broadcast_to(e, [tf.shape(x)[0], *e.shape])])
+		x = layers.Concatenate()([x, s])
+	
+		x = self.module_conv(x, 8*feature_multiplier, 8*feature_multiplier,
+			k1=(1,3), k2=(3,1), s1=(1,1), s2=(1,1), activation="tanh") #20x15
+
+
+		# self.model_image_decoder_o_image = layers.Conv2D(
+		# 	3, (1, 1), kernel_initializer=initializers.Orthogonal(0.01),
+		# 	activation="sigmoid")(x)
+
+		# self.model_image_decoder_o_depth = layers.Conv2D(
+		# 	1, (1, 1), kernel_initializer=initializers.Orthogonal(0.01),
+		# 	activation="linear")(x)
+
+
+		# x = self.module_interpolated_sin(x, n=16, upscale_ratio=2)
+		# x = self.module_interpolated_sin(x, n=8, upscale_ratio=2)
+		# x = self.module_interpolated_sin(x, n=4, upscale_ratio=2)
+		# x = self.module_interpolated_sin(x, n=2, upscale_ratio=2)
+
+
+		# x = self.module_interpolated_sin(x, n=6, upscale_ratio=16)
 
 		self.model_image_decoder_o_image = layers.Conv2D(
-			3, (1, 1), kernel_initializer=initializers.Orthogonal(0.01),
+			3, (1, 1), kernel_initializer=initializers.Orthogonal(0.1),
 			activation="sigmoid")(x)
 
 		self.model_image_decoder_o_depth = layers.Conv2D(
-			1, (1, 1), kernel_initializer=initializers.Orthogonal(0.01),
+			1, (1, 1), kernel_initializer=initializers.Orthogonal(0.1),
 			activation="linear")(x)
+
+
+		# self.model_image_decoder_o_image = x[:,:,:,0:3]
+		# self.model_image_decoder_o_depth = x[:,:,:,3:4]
 
 		self.model_image_decoder = keras.Model(
 			inputs=[self.model_image_decoder_i_image_enc],
