eb120bc57dc513da9af7a4af0829fbd6ffb2b01b
Lehdari <miika.lehtimaki@gmail.com>
2022-07-05 19:46:16 +0300

    image_loss.py: Implement laplacian pyramid loss

--- image_loss.py
+++ image_loss.py
@@ -2,65 +2,45 @@ import tensorflow as tf
 import numpy as np
 from tensorflow import keras
 from tensorflow.keras import layers
+from tensorflow_addons.image import gaussian_filter2d
 
 
 class ImageLoss:
-    def __init__ (self, image, image_pred, loss_mask=None, depth_channel=3, depth_weight=1.0):
-        if loss_mask is None:
-            loss_mask = tf.ones_like(image)
+    def __init__ (self, image, image_pred, loss_mask=None, depth_channel=3,
+        depth_weight=1.0, laplacian_weight=5.0):
+        self.loss_mask = loss_mask
+        if self.loss_mask is None:
+            self.loss_mask = tf.ones_like(image)
 
         self.depth_channel = depth_channel
         self.depth_weight = depth_weight
         depth_channel_mask_np = np.ones((image.shape[3]), dtype=np.float32)
         depth_channel_mask_np[depth_channel] = depth_weight
         self.depth_channel_mask = tf.convert_to_tensor(depth_channel_mask_np)
+        self.laplacian_weight = laplacian_weight
 
-        self.grad_x = []
-        self.grad_y = []
-        self.grad_x_pred = []
-        self.grad_y_pred = []
-        self.grad_x_mask = []
-        self.grad_y_mask = []
-        self.loss_grad = []
+        self.images_blurred = [image]
+        self.images_laplacian = []
+        self.image_preds_blurred = [image_pred]
+        self.image_preds_laplacian = []
 
-        grad_x, grad_y = ImageLoss.image_gradient(image)
-        grad_x_pred, grad_y_pred = ImageLoss.image_gradient(image_pred)
-        grad_x_mask, grad_y_mask = ImageLoss.gradient_masks(loss_mask)
-        loss_grad =\
-            self.image_loss(grad_x, grad_x_pred, grad_x_mask) +\
-            self.image_loss(grad_y, grad_y_pred, grad_y_mask)
+        self.losses_laplacian = []
         
-        self.grad_x.append(grad_x)
-        self.grad_y.append(grad_y)
-        self.grad_x_pred.append(grad_x_pred)
-        self.grad_y_pred.append(grad_y_pred)
-        self.grad_x_mask.append(grad_x_mask)
-        self.grad_y_mask.append(grad_y_mask)
-        self.loss_grad.append(loss_grad)
-
         self.loss_image = self.image_loss(image, image_pred, loss_mask)
 
-        self.images = [image]
-        self.image_preds = [image_pred]
-        self.loss_masks = [loss_mask]
-        for i in range(4):
-            self.images.append(layers.AveragePooling2D((2,2))(self.images[-1]))
-            self.image_preds.append(layers.AveragePooling2D((2,2))(self.image_preds[-1]))
-            self.loss_masks.append(layers.AveragePooling2D((2,2))(self.loss_masks[-1]))
-            grad_x, grad_y = ImageLoss.image_gradient(self.images[-1])
-            grad_x_pred, grad_y_pred = ImageLoss.image_gradient(self.image_preds[-1])
-            grad_x_mask, grad_y_mask = ImageLoss.gradient_masks(self.loss_masks[-1])
-            loss_grad =\
-                self.image_loss(grad_x, grad_x_pred, grad_x_mask) +\
-                self.image_loss(grad_y, grad_y_pred, grad_y_mask)
-            
-            self.grad_x.append(grad_x)
-            self.grad_y.append(grad_y)
-            self.grad_x_pred.append(grad_x_pred)
-            self.grad_y_pred.append(grad_y_pred)
-            self.grad_x_mask.append(grad_x_mask)
-            self.grad_y_mask.append(grad_y_mask)
-            self.loss_grad.append(loss_grad)
+        for i in range(5):
+            blur = 2.0**i
+            kernel_size = int(blur*4+1)
+
+            self.images_blurred.append(gaussian_filter2d(image,
+                filter_shape=[kernel_size, kernel_size], sigma=blur, padding="REFLECT"))
+            self.images_laplacian.append(self.images_blurred[-1]-self.images_blurred[-2])
+            self.image_preds_blurred.append(gaussian_filter2d(image_pred,
+                filter_shape=[kernel_size, kernel_size], sigma=blur, padding="REFLECT"))
+            self.image_preds_laplacian.append(self.image_preds_blurred[-1]-self.image_preds_blurred[-2])
+
+            self.losses_laplacian.append(self.laplacian_weight * self.image_loss(
+                self.images_laplacian[-1], self.image_preds_laplacian[-1], self.loss_mask))
         
     def image_loss(self, y_true, y_pred, mask):
         l1 = tf.reduce_mean(tf.abs(y_true - y_pred)*mask, axis=[0,1,2])
@@ -70,7 +50,7 @@ class ImageLoss:
 
     def total_loss(self):
         total_loss = self.loss_image
-        for l in self.loss_grad:
+        for l in self.losses_laplacian:
             total_loss += l
         return total_loss
     
